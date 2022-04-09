2eec97a6e8cbde9e7ed7c39aecec1ee4ee541765
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-04-09 01:02:50 +0000

    initial commit

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Jumbotron = () => {
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h5 class="my-4"></h5>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+}
+export default Jumbotron
